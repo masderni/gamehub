21bcac9627934b2e0a11861fa626894a9c1d1be3
Andri Mar <ams59@hi.is>
2025-04-18 01:39:55 +0000

    first commit

---
+++ runjar.cmd
@@ -0,0 +1,1 @@
+java -cp target\gamehub-1.0-SNAPSHOT.jar is.hi.hbv202g.project.App
---
+++ src/main/java/is/hi/hbv202g/project/App.java
@@ -0,0 +1,13 @@
+package is.hi.hbv202g.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
