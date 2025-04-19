3e85a05696ba1bdf95410da682d9a7a4beabfbae
Andri Mar <ams59@hi.is>
2025-04-19 20:40:36 +0000

    tic tac tests

--- src/test/java/is/hi/hbv202g/project/TicTacToeTest.java
+++ src/test/java/is/hi/hbv202g/project/TicTacToeTest.java
@@ -26,4 +26,24 @@ public class TicTacToeTest {
         ticTacToe.setBoard(board);
         assertTrue(ticTacToe.checkDraw());
     }
+
+    @Test
+    public void testwinfalse() {
+        TicTacToe ticTacToe = new TicTacToe();
+        char [][] board = {{'\0','\0','\0'},{'\0','\0','\0'},{'\0','\0','\0'}};
+        ticTacToe.setBoard(board);
+        assertFalse(ticTacToe.checkWin()); // bc the board needs to have a winner
+        char [][] board2 = {{'X','0','X'},{'X','O','X'},{'O','X','O'}};
+        ticTacToe.setBoard(board2);
+        assertFalse(ticTacToe.checkWin());
+    }
+
+    //simple win test
+    @Test
+    public void testwintrue() {
+        TicTacToe ticTacToe = new TicTacToe();
+        char [][] board = {{'X','X','X'},{'\0','\0','\0'},{'\0','\0','\0'}};
+        ticTacToe.setBoard(board);
+        assertTrue(ticTacToe.checkWin());
+    }
 }
